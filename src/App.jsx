411e3152a9943aaac6550dfd8ccba04278df8472
Gabriel Lira <103190663+GLira4k@users.noreply.github.com>
2023-05-16 23:06:13 -0300

    Update
    Feito o update do código que estava faltando

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,58 @@
 import { QRCodeSVG } from "qrcode.react";
-import "./App.css";
 import { useState } from "react";
+import Resizer from "react-image-file-resizer";
+import "./App.css";
 
 export default function App() {
-  const [logoFile, setLogoFile] = useState(null)
+  const [logoIcon, setLogoIcon] = useState("");
 
   function handleLogoFileChange(e) {
     const selectedFile = e.target.files[0];
-    if (selectedFile.type === "image/png") {
-      setLogoFile(selectedFile);
+    if (selectedFile && selectedFile.type === "image/png") {
+      resizeImage(selectedFile);
     } else {
       alert("Por favor, selecione um arquivo PNG");
     }
   }
 
+  function resizeImage(file) {
+    Resizer.imageFileResizer(
+      file,
+      400,
+      400,
+      "PNG",
+      100,
+      0,
+      (resizedImage) => {
+        const tempUrl = URL.createObjectURL(resizedImage);
+        setLogoIcon(tempUrl);
+      },
+      "blob"
+    );
+  }
 
   return (
     <main>
       <h1>Hello World!</h1>
-      <QRCodeSVG 
-        value="https://github.com.br/GLira4k"
-        size={250}
-        includeMargin={true}
-        imageSettings={{
-          src: logoFile ? URL.createObjectURL(logoFile) : "",
-          x: undefined,
-          y: undefined,
-          height: 10,
-          width: 40,
-          excavate: true,
-        }}
-      />
-
-      <input type="file" name="icon" id="icon" accept=".png" onChange={handleLogoFileChange}/>
+      <div className="qrcode-container">
+        <QRCodeSVG
+          value="https://github.com.br/GLira4k"
+          size={250}
+          includeMargin={true}
+          imageSettings={{
+            src: logoIcon || "",
+            x: undefined,
+            y: undefined,
+            width: 40,
+            height: 40,
+            excavate: true,
+            style: {
+              borderRadius: "5px",
+            },
+          }}
+        />
+      </div>
+      <input type="file" name="icon" id="icon" accept=".png" onChange={handleLogoFileChange} />
     </main>
   );
 }
